544fdfef3c45a73f669521360cfc81c5b6641267
smalugu-sofi <smalugu@sofi.org>
2020-12-31 18:12:46 -0800

    part2-add icons

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Startup name generator',
+      home: RandomWords(),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _saved = Set<WordPair>();
+  final _biggerFont = TextStyle(fontSize: 18.0);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup name generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if(i.isOdd) return Divider();
+
+        final index = i ~/2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index], index);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair, int index) {
+    final alreadySaved = _saved.contains(pair);
+    return ListTile(
+      title: Text(
+        index.toString() + ". " + pair.first + " " + pair.second,
+        style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite: Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+    );
+  }
+}
+
